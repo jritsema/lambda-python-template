b9daed9cc321697eadc15ca4975f5d6f54d53bf9
John Ritsema <john_ritsema@yahoo.com>
2025-09-17 17:53:26 -0400

    upgrades to python 3.13

--- zip.sh
+++ zip.sh
@@ -2,7 +2,7 @@
 set -e
 
 pip install -r requirements.txt
-cd .venv/lib/python3.9/site-packages
+cd .venv/lib/python3.13/site-packages
 zip -r ../../../../lambda.zip .
 cd ../../../../
 zip -g lambda.zip *.py
